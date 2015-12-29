ee9afccf526f4cb77db19a074786ad60f5c7253c
kevincwuu <kevincwuu@gmail.com>
2015-12-28 20:00:36 -0800

    Initial Commit

---
+++ Pokédex-by-Kevin-Wu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokédex-by-Kevin-Wu
+//
+//  Created by Kevin Wu on 12/28/15.
+//  Copyright (c) 2015 Kevin Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
